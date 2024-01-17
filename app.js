732efe8f80086200059dc862ab5048d75a168f8a
DB5640 <122465471+DB5640@users.noreply.github.com>
2024-01-17 13:45:21 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+
+let numeroSecreto = "Si";
+alert("Hola Mundo");
+let numeroUsuario = prompt("Dime si me amas (Si o No):")
+console.log(" "+numeroUsuario);
+
+if(numeroUsuario == numeroSecreto){
+    alert ("Me parece muy bien mi amorcito");
+    
+}
+else {"Como que no!!😭😭"};
